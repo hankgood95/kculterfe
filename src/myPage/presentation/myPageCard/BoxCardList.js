0e45c18fdb038338d0ebb7368cafeaaa3c9397f9
guswlsakfls <guswlsakfls@gmail.com>
2022-09-09 08:48:18 +0900

    Style: 코스 추가 버튼 업데이트
    - 누르면 현재 리덕스 저장 정보 가지고 맵 페이지로 이동

--- src/myPage/presentation/myPageCard/BoxCardList.js
+++ src/myPage/presentation/myPageCard/BoxCardList.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
 import BoxCardItem from './BoxCardItem';
 import { getCourseList } from '../../container/GetCours';
 import styled from 'styled-components';
@@ -21,24 +22,31 @@ function BoxCardList() {
 	return (
 		<div className="box-list">
 			{ data&&data.map((course, index) => <BoxCardItem key={index} props={course} dayNum={dayNum++} {...course} />) || <div>Please add course.</div> }
-			<AddtoCouseBtn>
-                        +
-            </AddtoCouseBtn>
+			<Link className='text-black' to='/MapPage'>
+				<AddtoCouseBtn>
+					+
+				</AddtoCouseBtn>
+			</Link>
 		</div>
 	)
 }
 
-const AddtoCouseBtn = styled.div`
+const AddtoCouseBtn = styled.span`
     font-size: 20px;
     position: absolute;
-    right: 80px;
-    bottom: 470px;
+    right: 100px;
+    bottom: 80px;
     border-radius: 50%;
-    border: 1px solid white;
-	box-shadow: 1px;
+    border: 1px solid gray;
+	box-shadow: 0px 0px 4px 0.1px gray;
     padding: 1px 10px 3.5px 12px;
-    outline: 1px solid black;
+    outline: 2px solid black;
     outline-offset: -9px;
+
+	&:hover {
+		background: #f4029b;
+		cursor: pointer;
+	}
 `
 
 export default BoxCardList;
