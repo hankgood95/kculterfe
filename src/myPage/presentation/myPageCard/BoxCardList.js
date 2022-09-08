d220ddfba72987ffe8b386e2ce0898f1820e7741
guswlsakfls <guswlsakfls@gmail.com>
2022-09-08 21:51:41 +0900

    Style: for push

--- src/myPage/presentation/myPageCard/BoxCardList.js
+++ src/myPage/presentation/myPageCard/BoxCardList.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import BoxCardItem from './BoxCardItem';
 import { getCourseList } from '../../container/GetCours';
+import styled from 'styled-components';
 
 function BoxCardList() {
 	let dayNum = 1;
@@ -20,8 +21,24 @@ function BoxCardList() {
 	return (
 		<div className="box-list">
 			{ data&&data.map((course, index) => <BoxCardItem key={index} props={course} dayNum={dayNum++} {...course} />) || <div>Please add course.</div> }
+			<AddtoCouseBtn>
+                        +
+            </AddtoCouseBtn>
 		</div>
 	)
 }
+
+const AddtoCouseBtn = styled.div`
+    font-size: 20px;
+    position: absolute;
+    right: 80px;
+    bottom: 470px;
+    border-radius: 50%;
+    border: 1px solid white;
+	box-shadow: 1px;
+    padding: 1px 10px 3.5px 12px;
+    outline: 1px solid black;
+    outline-offset: -9px;
+`
 
 export default BoxCardList;
