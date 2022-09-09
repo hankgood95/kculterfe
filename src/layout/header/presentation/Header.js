81d3d9db5e7bee13a9c2085b09fd56f885d16d42
jeonjeunghoon <jeonjeunghoon@gmail.com>
2022-09-10 02:48:07 +0900

    Update: CSS

--- src/layout/header/presentation/Header.js
+++ src/layout/header/presentation/Header.js
@@ -6,9 +6,15 @@ import {
 	Link,
 	useLocation,
 } from 'react-router-dom';
+import {
+	useSelector
+} from 'react-redux';
 import OffCanvasSidebar from '../../sidebar/offCanvasSidebar/offCanvasSidebar';
 import logoBlack from '../../../src_asset/loginblack.png';
 import './Header.css';
+import map from '../../../src_asset/map.svg';
+import mypage from '../../../src_asset/gear.svg';
+import noImage from '../../../src_asset/no_img.png';
 
 function Header(props) {
 	const location = useLocation();
@@ -17,34 +23,37 @@ function Header(props) {
 	const [isOpen, setOpen] = useState(true);
 	const sideClose = () => setOpen(false);
 	const handleOpen = (e) => setOpen(e);
+	const idolSelected = useSelector(state => state.idolSelected);
+	const attrSelected = useSelector(state => state.attrSelected);
+	const concertSelected = useSelector(state => state.concertSelected);
+	const dashboardSelected = useSelector(state => state.dashboardSelected);
+	const settingSelected = useSelector(state => state.settingSelected);
 
 	useEffect(() => {
-		switch (location.pathname) {
-			case '/MapPage':
-				setTitle((title) => "Map");
-				setImg((img) => "https://i.pinimg.com/originals/7f/b0/c9/7fb0c94c6252c18e16ec4bde430cdf2b.png");
-				break;
-			case '/Mypage':
-				setTitle((title) => "My Page");
-				setImg((img) => "");
-				break;
-			case '/IdolListPage':
-				setTitle((title) => "K-Culter");
-				setImg((img) => "");
-				break;
-			case '/ConcertPage':
-				setTitle((title) => "K-Culter");
-				setImg((img) => "");
-				break;
-			case '/CulturePage':
-				setTitle((title) => "K-Culter");
-				setImg((img) => "");
-				break;
-			default:
-				setTitle((title) => "Error");
-				setImg((img) => "");
+		console.log(idolSelected, attrSelected, concertSelected, dashboardSelected, settingSelected);
+		if (location.pathname == "/MapPage") {
+			setTitle((title) => "Map");
+			setImg((img) => map);
+		} else if (location.pathname == "/IdolListPage" && idolSelected) {
+			setTitle((title) => "Kpop");
+			setImg((img) => "https://i.pinimg.com/originals/7f/b0/c9/7fb0c94c6252c18e16ec4bde430cdf2b.png");
+		} else if (location.pathname == "/IdolListPage" && attrSelected) {
+			setTitle((title) => "Culture");
+			setImg((img) => "https://i.pinimg.com/originals/7f/b0/c9/7fb0c94c6252c18e16ec4bde430cdf2b.png");
+		} else if (location.pathname == "/IdolListPage" && concertSelected) {
+			setTitle((title) => "Concert");
+			setImg((img) => "https://i.pinimg.com/originals/7f/b0/c9/7fb0c94c6252c18e16ec4bde430cdf2b.png");
+		} else if (location.pathname == "/Mypage" && dashboardSelected) {
+			setTitle((title) => "Dashboard");
+			setImg((img) => mypage);
+		} else if (location.pathname == "/Mypage" && settingSelected) {
+			setTitle((title) => "Setting");
+			setImg((img) => mypage);
+		} else {
+			setTitle((title) => "Error");
+			setImg((img) => noImage);
 		}
-	}, [location.pathname]);
+	}, [idolSelected, attrSelected, concertSelected, dashboardSelected, settingSelected, location.pathname]);
 
 	useEffect(() => {
 		props.handleOpen(isOpen);
