a823533c1e456b806a52fc71cb17d74741ded0e7
hankgood95 <hankgood958@gmail.com>
2022-09-10 23:39:41 +0900

    백엔드 경로 수정

--- src/login/container/Loginbtn.js
+++ src/login/container/Loginbtn.js
@@ -7,7 +7,7 @@ import axios from 'axios';
 
 function Loginbtn (props) {
   //이 멤버를 세션 스토리지에 저장을 해야한다. 
-  axios.defaults.baseURL = 'http://3.37.88.220:8080';
+  axios.defaults.baseURL = 'http://kculter-lb-1250111111.ap-northeast-2.elb.amazonaws.com/';
 
   const sendToServer = () => {
     if (props.email === ""){
