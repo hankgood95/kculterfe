a6a7bac472ec8021bd970d662d4f363a4ba35021
hankgood95 <hankgood958@gmail.com>
2022-09-11 05:44:16 +0900

    수정...

--- src/login/container/Loginbtn.js
+++ src/login/container/Loginbtn.js
@@ -7,7 +7,7 @@ import axios from 'axios';
 
 function Loginbtn (props) {
   //이 멤버를 세션 스토리지에 저장을 해야한다. 
-  axios.defaults.baseURL = 'https://kculter-lb-1250111111.ap-northeast-2.elb.amazonaws.com';
+  axios.defaults.baseURL = 'http://kculter-lb-1250111111.ap-northeast-2.elb.amazonaws.com';
 
   const sendToServer = () => {
     if (props.email === ""){
