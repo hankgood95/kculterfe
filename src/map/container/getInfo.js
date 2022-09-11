9583806ebc277e94597137620c6199e59626e755
jeonjeunghoon <jeonjeunghoon@gmail.com>
2022-09-12 04:45:24 +0900

    update: axios

--- src/map/container/getInfo.js
+++ src/map/container/getInfo.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export async function getPlaceApi(url, type, keyHash) {
-	axios.defaults.baseURL = 'https://kculter-be-lb-830632987.ap-northeast-2.elb.amazonaws.com';
+	axios.defaults.baseURL = 'https://wooks-weather.com';
 	return await axios.get(url + "?keyhash=" + keyHash + "&type=" + type)
 	.then(function(res) {
 		return res;
@@ -13,7 +13,7 @@ export async function getPlaceApi(url, type, keyHash) {
 }
 
 export async function getPinApi(url, type, keyHash) {
-	axios.defaults.baseURL = 'https://kculter-be-lb-830632987.ap-northeast-2.elb.amazonaws.com';
+	axios.defaults.baseURL = 'https://wooks-weather.com';
 	return await axios.get(url + type + "?keyHash=" + keyHash)
 	.then(function(res) {
 		return res;
